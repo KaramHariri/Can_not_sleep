a2d661fe004e1a5f036cb540a97847c19cd0910f
Migge <miggex91@hotmail.com>
2021-02-25 23:25:38 +0100

    Buncha stuff added
    I'm getting hungry just watching all this spaghetti code :)

---
+++ Can_not_sleep/Assets/Scripts/Star/Star.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem glowSystem;
+
+    private float appearTime = 0.4f;
+
+    public enum StarState 
+    {
+        Inactive,
+        Active
+    }
+
+    private StarState previousState = StarState.Inactive; 
+    [SerializeField]
+    private StarState currentState = StarState.Inactive;
+
+    void Awake()
+    {
+        glowSystem = transform.Find("Glow").GetComponent<ParticleSystem>();
+        if (glowSystem == null) { Debug.Log("Could not find glow particle System! "); }
+    }
+
+    void Update()
+    {
+        if (currentState != previousState)
+        {
+            previousState = currentState;
+
+            if(currentState == StarState.Active)
+            {
+                activateGlow();
+            }
+            else
+            {
+                deactivateGlow();
+            }
+        }
+
+        //if(Input.GetKeyDown(KeyCode.U))
+        //{
+        //    ActivateAppearance();
+        //}
+
+
+        //if (Input.GetKeyDown(KeyCode.O))
+        //{
+        //    ActivateDisappearance();
+        //}
+    }
+
+    public bool isActive()
+    {
+        return (currentState == StarState.Active);
+    }
+
+    public void activateGlow()
+    {
+        Debug.Log("Activated Glow");
+        glowSystem.Play();
+    }
+
+    void deactivateGlow()
+    {
+        Debug.Log("Deactivated Glow");
+        glowSystem.Clear();
+        glowSystem.Stop();
+    }
+
+    public void ActivateAppearance()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Appear());
+    }
+
+    public void ActivateDisappearance()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Disappear());
+    }
+
+    IEnumerator Appear()
+    {
+        float currentTime = 0;
+
+        while (currentTime < appearTime)
+        {
+            currentTime += Time.deltaTime;
+            if(currentTime > appearTime) { currentTime = appearTime; }
+            transform.localScale = new Vector3(CubicEaseIn(currentTime, 0, 1, appearTime), CubicEaseIn(currentTime, 0, 1, appearTime), 1);
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+    IEnumerator Disappear()
+    {
+        float currentTime = 0;
+
+        while (currentTime < appearTime)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime > appearTime) { currentTime = appearTime; }
+            transform.localScale = new Vector3(CubicEaseOut(currentTime, 1, -1, appearTime), CubicEaseOut(currentTime, 1, -1, appearTime), 1);
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+    //Recall that t is time,b is beginning position,c is the total change in position, and d is the duration of the tween.
+
+    float CubicEaseIn( float t, float b, float c, float d )
+    {
+        return c * (t /= d) * t * t + b;
+    }
+    float CubicEaseOut( float t, float b, float c, float d )
+    {
+        return c * ((t = t / d - 1) * t * t + 1) + b;
+    }
+
+}
